03d358589230e79bd272c02c7a821028569cc9fb
thofschuster <tim.hofschuster@googlemail.com>
2017-02-16 16:46:07 +0100

    push after npm install. not sure if this is good.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My first Angular App!</h1>`
+    template: `<h1>Hello</h1>`
 })
 export class AppComponent {
-  
+
 }
